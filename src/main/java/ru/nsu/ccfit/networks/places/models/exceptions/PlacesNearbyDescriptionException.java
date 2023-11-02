c9bd5644c58c81ba04f673e17573ec0efbede000
ptrvsrg <s.petrov1@g.nsu.ru>
2023-11-03 00:55:55 +0700

    Add exception

---
+++ src/main/java/ru/nsu/ccfit/networks/places/models/exceptions/PlacesNearbyDescriptionException.java
@@ -0,0 +1,13 @@
+package ru.nsu.ccfit.networks.places.models.exceptions;
+
+public class PlacesNearbyDescriptionException
+    extends Exception {
+
+    public PlacesNearbyDescriptionException() {
+        super();
+    }
+
+    public PlacesNearbyDescriptionException(String message) {
+        super(message);
+    }
+}
